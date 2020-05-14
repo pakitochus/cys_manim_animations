48b7871ef26b39de5d8ca529981e04a04ee262c7
Francisco Jesús Martínez Murcia <pakitochus@gmail.com>
2020-05-14 18:19:19 +0200

    Optimizado el primer video, Curva, para sincronía con la narración

--- cys.py
+++ cys.py
@@ -10,7 +10,7 @@ from manimlib.imports import *
 
 
 t_offset = 0
-rate = 0.05
+rate = 0.1
 # global w
 w = 1
 a_out = 1
@@ -49,40 +49,39 @@ class Curve(Scene):
         eq2.shift(2*UP).shift(LEFT*FRAME_WIDTH/4)
         eq3=TextMobject('$A_{\\omega_1}sin(\\omega_1 t + \\phi_{\\omega_1})$')
         eq3.shift(2*UP).shift(RIGHT*(FRAME_WIDTH/4+0.5))
-        eq4=TextMobject('$A_{\\omega_1}=|H(j\\omega_1)|$')
-        eq4.shift(2*DOWN)
-        eq5=TextMobject('$\\phi_{\\omega_1}=\\angle H(j\\omega_1)$')
-        eq5.shift(3*DOWN)
+        # eq4=TextMobject('$A_{\\omega_1}=|H(j\\omega_1)|$')
+        # eq4.shift(2*DOWN)
+        # eq5=TextMobject('$\\phi_{\\omega_1}=\\angle H(j\\omega_1)$')
+        # eq5.shift(3*DOWN)
         
         eq2_2=TextMobject("$\sin(\\omega_2 t)$")
         eq2_2.shift(2*UP).shift(LEFT*FRAME_WIDTH/4)
         eq3_2=TextMobject('$A_{\\omega_2}sin(\\omega_2 t + \\phi_{\\omega_2})$')
         eq3_2.shift(2*UP).shift(RIGHT*(FRAME_WIDTH/4+0.5))
-        eq4_2=TextMobject('$A_{\\omega_2}=|H(j\\omega_2)|$')
-        eq4_2.shift(2*DOWN)
-        eq5_2=TextMobject('$\\phi_{\\omega_2}=\\angle H(j\\omega_2)$')
-        eq5_2.shift(3*DOWN)
+        # eq4_2=TextMobject('$A_{\\omega_2}=|H(j\\omega_2)|$')
+        # eq4_2.shift(2*DOWN)
+        # eq5_2=TextMobject('$\\phi_{\\omega_2}=\\angle H(j\\omega_2)$')
+        # eq5_2.shift(3*DOWN)
         
         c = get_sin(a_out, w, shift=RIGHT)
         c.add_updater(update_curve)
         c2 = get_sin(1, w, shift=LEFT)
         c2.add_updater(update_curve2)
         
-        self.play(ShowCreation(square))
-        self.play(Write(eq1))
+        self.play(ShowCreation(square),Write(eq1))
         self.play(ShowCreation(c2))
         self.play(Write(eq2))
         rate=rate/2
         self.play(ShowCreation(c))
         self.play(Write(eq3))
         self.wait(2)
-        self.play(Write(eq4),Write(eq5))
-        self.wait(3)
+        # self.play(Write(eq4),Write(eq5))
+        # self.wait(3)
         
         # Transform equations
-        self.play(ReplacementTransform(eq2, eq2_2), ReplacementTransform(eq3, eq3_2),
-                  ReplacementTransform(eq4, eq4_2), ReplacementTransform(eq5, eq5_2))
-        self.wait(1)
+        self.play(ReplacementTransform(eq2, eq2_2), ReplacementTransform(eq3, eq3_2))#,
+                  # ReplacementTransform(eq4, eq4_2), ReplacementTransform(eq5, eq5_2))
+        # self.wait(1)
         w = w*2
         a_out = 0.7
         self.wait(3)
@@ -92,13 +91,13 @@ class Curve(Scene):
         eq2_3.shift(2*UP).shift(LEFT*FRAME_WIDTH/4)
         eq3_3=TextMobject('$A_{\\omega_3}sin(\\omega_3 t + \\phi_{\\omega_3})$')
         eq3_3.shift(2*UP).shift(RIGHT*(FRAME_WIDTH/4+0.5))
-        eq4_3=TextMobject('$A_{\\omega_3}=|H(j\\omega_3)|$')
-        eq4_3.shift(2*DOWN)
-        eq5_3=TextMobject('$\\phi_{\\omega_3}=\\angle H(j\\omega_3)$')
-        eq5_3.shift(3*DOWN)
-        self.play(ReplacementTransform(eq2_2, eq2_3), ReplacementTransform(eq3_2, eq3_3),
-                  ReplacementTransform(eq4_2, eq4_3), ReplacementTransform(eq5_2, eq5_3))
-        self.wait(0.5)
+        # eq4_3=TextMobject('$A_{\\omega_3}=|H(j\\omega_3)|$')
+        # eq4_3.shift(2*DOWN)
+        # eq5_3=TextMobject('$\\phi_{\\omega_3}=\\angle H(j\\omega_3)$')
+        # eq5_3.shift(3*DOWN)
+        self.play(ReplacementTransform(eq2_2, eq2_3), ReplacementTransform(eq3_2, eq3_3))#,
+                  # ReplacementTransform(eq4_2, eq4_3), ReplacementTransform(eq5_2, eq5_3))
+        # self.wait(0.5)
         w = w*1.5
         a_out = 0.3
         self.wait(1)
@@ -108,51 +107,48 @@ class Curve(Scene):
         eq2_4.shift(2*UP).shift(LEFT*FRAME_WIDTH/4)
         eq3_4=TextMobject('$A_{\\omega_4}sin(\\omega_4 t + \\phi_{\\omega_4})$')
         eq3_4.shift(2*UP).shift(RIGHT*(FRAME_WIDTH/4+0.5))
-        eq4_4=TextMobject('$A_{\\omega_4}=|H(j\\omega_4)|$')
-        eq4_4.shift(2*DOWN)
-        eq5_4=TextMobject('$\\phi_{\\omega_4}=\\angle H(j\\omega_4)$')
-        eq5_4.shift(3*DOWN)
-        self.play(ReplacementTransform(eq2_3, eq2_4), ReplacementTransform(eq3_3, eq3_4),
-                  ReplacementTransform(eq4_3, eq4_4), ReplacementTransform(eq5_3, eq5_4))
-        self.wait(0.5)
+        # eq4_4=TextMobject('$A_{\\omega_4}=|H(j\\omega_4)|$')
+        # eq4_4.shift(2*DOWN)
+        # eq5_4=TextMobject('$\\phi_{\\omega_4}=\\angle H(j\\omega_4)$')
+        # eq5_4.shift(3*DOWN)
+        self.play(ReplacementTransform(eq2_3, eq2_4), ReplacementTransform(eq3_3, eq3_4))#,
+                  # ReplacementTransform(eq4_3, eq4_4), ReplacementTransform(eq5_3, eq5_4))
+        # self.wait(0.5)
         w = w*2
         a_out = 0.05
         self.wait(2)
         
         # Transform equations
-        self.play(ReplacementTransform(eq2_4, eq2_2), ReplacementTransform(eq3_4, eq3_2),
-                  ReplacementTransform(eq4_4, eq4_2), ReplacementTransform(eq5_4, eq5_2))
-        self.wait(0.5)
-        w = w/(2*1.5)
-        a_out = 0.7
+        # self.play(ReplacementTransform(eq2_4, eq2_2), ReplacementTransform(eq3_4, eq3_2),
+        #           ReplacementTransform(eq4_4, eq4_2), ReplacementTransform(eq5_4, eq5_2))
+        # self.wait(0.5)
+        # w = w/(2*1.5)
+        # a_out = 0.7
         for i in range(10):
             self.wait(0.1)
             rate=rate*1/(i+1)
         
-        # VOY POR AQUI.. 
-        text = TexMobject("y(t)", "=", "A_{\\omega_2}", "\\sin","(", "\\omega_2","t", "+","\\phi_{\\omega_2}",")")
+        
+        text = TexMobject("y(t)", "=", "A_{\\omega_i}", "\\sin","(", "\\omega_i","t", "+","\\phi_{\\omega_i}",")")
         text.shift(1*DOWN)
         self.remove(c)
         self.remove(c2)
         self.play(ApplyMethod(eq1.shift, 2*UP),
                   ApplyMethod(square.shift, 2*UP))
-        self.play(FadeOut(eq2_2), FadeOut(eq3_2), FadeOut(eq4_2), 
-                  FadeOut(eq5_2), Write(text))
+        self.play(FadeOut(eq2_4), FadeOut(eq3_4), Write(text)) # , FadeOut(eq4_2), FadeOut(eq5_2), 
         
         self.play(ApplyMethod(text[2].set_color, YELLOW))
         self.play(ApplyMethod(text[5].set_color, RED))
         self.play(ApplyMethod(text[8].set_color, GREEN))
         brace_top = Brace(text[2], UP, buff = SMALL_BUFF)
         brace_bottom = Brace(text[8], DOWN, buff = SMALL_BUFF)
-        text_top = brace_top.get_text("$|H(j\\omega)|$")
-        text_bottom = brace_bottom.get_text("$\\angle H(j\\omega)$")
-        self.play(
-            GrowFromCenter(brace_top),
-            GrowFromCenter(brace_bottom),
-            FadeIn(text_top),
-            FadeIn(text_bottom)
-            )
+        text_top = brace_top.get_text("$|H(j\\omega_i)|$")
+        text_top.set_color(YELLOW)
+        text_bottom = brace_bottom.get_text("$\\angle H(j\\omega_i)$")
+        text_bottom.set_color(GREEN)
+        self.play(GrowFromCenter(brace_top),FadeIn(text_top))
         self.wait(2)
+        self.play(GrowFromCenter(brace_bottom),FadeIn(text_bottom))
         # self.play(ApplyMethod(eq6.set_color_by_tex, "A", RED, substring=True))
         # eq6.set_color_by_tex(tex, color)
         self.wait(5)
